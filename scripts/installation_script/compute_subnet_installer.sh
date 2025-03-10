ec6bec3ba4dfb0fca2307e94d33c123d10251644
Eudardo <eduardonicol3it@gmail.com>
2025-03-10 05:17:57 -0300

    chore: update confirm statement

--- scripts/installation_script/compute_subnet_installer.sh
+++ scripts/installation_script/compute_subnet_installer.sh
@@ -694,23 +694,23 @@ echo "Use 'pm2 logs subnet${NETUID}_miner' to see logs, or check ${CS_PATH}/pm2_
 if $AUTOMATED; then
   pm2 save
   sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
-  else
-    echo "To ensure that your miner automatically restarts after a system reboot,"
-    echo "we will configure PM2 to save the current process list and resurrect it upon system start."
-    echo "Would you like to enable this feature?(Yes/No)"
-    select yn in "Yes" "No"; do
-      case $yn in
-        Yes )
-          pm2 save
-          sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
-          break
-          ;;
-        No )
-          break
-          ;;
-      esac
-    done
-  fi
+else
+  echo "To ensure that your miner automatically restarts after a system reboot,"
+  echo "It will configure PM2 to save the current process list and resurrect it upon system start."
+  echo "Would you like to enable this feature?(Yes/No)"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes )
+        pm2 save
+        sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
+        break
+        ;;
+      No )
+        break
+        ;;
+    esac
+  done
+fi
 echo
 echo "NOTE: If you have not yet created and funded a wallet (coldkey/hotkey),"
 echo "the miner will fail to serve until you do so and specify those keys."
