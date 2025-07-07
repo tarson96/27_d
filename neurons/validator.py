66bebadd7554efd0bde7b4f0eb3efd402714ce7f
David Maldonado <gorossy07@gmail.com>
2025-07-07 09:49:18 -0500

    fix(CSN-888): solve problem in validator healthcheck

--- neurons/validator.py
+++ neurons/validator.py
@@ -847,17 +847,6 @@ class Validator:
             host = miner_info['host']
             bt.logging.debug(f"{hotkey}: Allocated Miner for testing.")
 
-            # Check fixed_external_user_port
-            try:
-                fixed_external_user_port = miner_info.get('fixed_external_user_port', 27015)
-                response = requests.get(f"http://{host}:{fixed_external_user_port}", timeout=2)
-                if response.status_code != 200:
-                    bt.logging.info(f"{hotkey}: Port {fixed_external_user_port} HTTP server not responding correctly")
-                    return (hotkey, None, -1)
-            except requests.exceptions.RequestException as e:
-                bt.logging.info(f"{hotkey}: Error connecting to port {fixed_external_user_port}: {e}")
-                return (hotkey, None, -1)
-
             # Step 2: Connect via SSH
             ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -1039,7 +1028,6 @@ class Validator:
                         miner_info = {
                             'host': axon.ip,
                             'port': info['port'],
-                            'fixed_external_user_port': info.get('fixed_external_user_port'),
                             'username': info['username'],
                             'password': info['password'],
                             'fixed_external_user_port': info.get('fixed_external_user_port', 27015),
